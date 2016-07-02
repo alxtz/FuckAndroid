d9075462f5fdc61b668cc2f22744b2c2ea2c5d75
Bj <fate412460@gmail.com>
2016-07-03 02:26:33 +0800

    超屌版本

--- app/src/main/java/com/example/bj/calendarapp1/MainActivity.java
+++ app/src/main/java/com/example/bj/calendarapp1/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.bj.calendarapp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
 }
